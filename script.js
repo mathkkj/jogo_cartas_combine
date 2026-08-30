79208c5768eeb2f8de169355dc3bab5fd77d4b70
mathkkj <matheus.cabral24@nave.org.br>
2026-08-30 18:58:57 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,59 @@
+let rodada = 1;
+let tempo = 45;
+let intervalo;
+
+let pontos = [8, 6, 4, 2];
+
+function iniciarTempo() {
+    clearInterval(intervalo);
+
+    tempo = 45;
+    document.getElementById("tempo").textContent = tempo;
+
+    intervalo = setInterval(() => {
+        tempo--;
+
+        document.getElementById("tempo").textContent = tempo;
+
+        if (tempo <= 0) {
+            clearInterval(intervalo);
+            document.getElementById("tempo").textContent = "Fim!";
+        }
+    }, 1000);
+}
+
+function proximaRodada() {
+    rodada++;
+
+    document.getElementById("rodada").textContent = rodada;
+
+    iniciarTempo();
+}
+
+function alterarPontos(jogador, valor) {
+    pontos[jogador - 1] += valor;
+
+    if (pontos[jogador - 1] < 0) {
+        pontos[jogador - 1] = 0;
+    }
+
+    document.getElementById("pontos" + jogador).textContent =
+        pontos[jogador - 1] + " pts";
+}
+
+function resetarJogo() {
+    clearInterval(intervalo);
+
+    rodada = 1;
+    tempo = 45;
+
+    pontos = [0, 0, 0, 0];
+
+    document.getElementById("rodada").textContent = rodada;
+    document.getElementById("tempo").textContent = tempo;
+
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById("pontos" + i).textContent =
+            pontos[i - 1] + " pts";
+    }
+}
